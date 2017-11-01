c7f8925151da400b06430efa1e731408a991bc2b
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-11-01 23:01:43 +0700

    introduces router

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { AppContainer } from 'react-hot-loader';
+import { Router } from 'react-router-dom';
 import { Button } from 'semantic-ui-react';
 import configureStore from './store/configureStore';
+import history from './history'
 import Vet from './vet';
 
 import 'semantic-ui-css/semantic.min.css';
@@ -21,7 +23,9 @@ const render = Component => {
   ReactDOM.render(
     <AppContainer>
       <Provider store={store}>
-        <Component/>
+        <Router history={history}>
+          <Component/>
+        </Router>
       </Provider>
     </AppContainer>,
     document.getElementById('root')
