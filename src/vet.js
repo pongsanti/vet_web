bd02e7aee03ccfac51ba42af6a1a05c82a3b1ba9
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-11-13 23:15:57 +0700

    select role at the home page

--- src/vet.js
+++ src/vet.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
-import { Menu, Modal, Container, Dropdown, Image, Header, Button, Icon } from 'semantic-ui-react';
+import { Menu, Modal, Container, Dropdown, Image, Header, Button, Icon, Segment, Divider } from 'semantic-ui-react';
 import Doctor from './components/doctor';
 import Vehicle from './components/vehicle';
 import DoctorApp from './components/doctor_app';
@@ -9,13 +9,15 @@ import VehicleApp from './components/vehicle_app';
 import {USER_EMAIL} from './helpers/user';
 
 import history from './history';
-import {doctorGet} from './actions'
 
 import LOGO from './assets/HTML5_Badge_256.png';
 
 import moment from 'moment';
 import BigCalendar from 'react-big-calendar';
 BigCalendar.momentLocalizer(moment);
+
+const ROLE_ADMIN = 'ADMIN';
+const ROLE_USER = 'USER';
 
 const mapStateToProps = state => {
   const {ui} = state
@@ -25,17 +27,31 @@ const mapStateToProps = state => {
 }
 
 class Vet extends Component {
-  componentWillMount () {
-    const {dispatch} = this.props;
-    dispatch(doctorGet());
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      role: null
+    }
   }
 
   onMenuClick (link) {
     history.push(`/${link}`)
   }
 
+  onLoginRoleClick (role) {
+    this.setState({ role });
+    if (role === ROLE_ADMIN) {
+      history.push(`/doctors`);
+    } else if (role == ROLE_USER) {
+      history.push(`/doctor_apps`);
+    }
+  }
+
   render () {
     const {load} = this.props;
+    const {role} = this.state;
+    const role_admin = role === ROLE_ADMIN;
     return (
       <div>
         <Modal open={load} size='mini'>
@@ -45,41 +61,56 @@ class Vet extends Component {
             </Container>
           </Modal.Content>
         </Modal>
-        <Menu fixed='top' inverted>
-          <Container>
-            <Menu.Item as='a' header>
-              <Image
-                size='mini'
-                src={LOGO}
-                style={{ marginRight: '1.5em' }}
-              />
-              Vet Appointments
-            </Menu.Item>
-            <Menu.Item as='a' onClick={this.onMenuClick.bind(this, 'doctors')}><Icon name='doctor' /> Doctors</Menu.Item>
-            <Menu.Item as='a' onClick={this.onMenuClick.bind(this, 'vehicles')}><Icon name='shipping' /> Vehicles</Menu.Item>
-            <Menu.Item as='a' onClick={this.onMenuClick.bind(this, 'doctor_apps')}><Icon name='calendar' /> Doctor Apps</Menu.Item>
-            <Menu.Item as='a' onClick={this.onMenuClick.bind(this, 'vehicle_apps')}><Icon name='calendar' /> Vehicle Apps</Menu.Item>
-            
-            <Menu.Menu position='right'>
-              <Menu.Item className='item'>
-                <Icon name='user' />
+        {role && 
+          <Menu fixed='top' inverted>
+            <Container>
+              <Menu.Item as='a' header onClick={this.onLoginRoleClick.bind(this, null)}>
+                <Image
+                  size='mini'
+                  src={LOGO}
+                  style={{ marginRight: '1.5em' }}
+                />
+                Home
               </Menu.Item>
-              <Dropdown text={USER_EMAIL} pointing className='link item'>
-                <Dropdown.Menu>
-                  <Dropdown.Item disabled>Profile</Dropdown.Item>
-                  <Dropdown.Item disabled>Log out</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </Menu.Menu>
+              { role_admin && <Menu.Item as='a' onClick={this.onMenuClick.bind(this, 'doctors')}><Icon name='doctor' /> Doctors</Menu.Item> }
+              { role_admin && <Menu.Item as='a' onClick={this.onMenuClick.bind(this, 'vehicles')}><Icon name='shipping' /> Vehicles</Menu.Item> }
+              <Menu.Item as='a' onClick={this.onMenuClick.bind(this, 'doctor_apps')}><Icon name='calendar' /> Doctor Apps</Menu.Item>
+              <Menu.Item as='a' onClick={this.onMenuClick.bind(this, 'vehicle_apps')}><Icon name='calendar' /> Vehicle Apps</Menu.Item>
+              
+              <Menu.Menu position='right'>
+                <Menu.Item className='item'>
+                  <Icon name='user' />
+                </Menu.Item>
+                <Dropdown text={USER_EMAIL} pointing className='link item'>
+                  <Dropdown.Menu>
+                    <Dropdown.Item disabled>Profile</Dropdown.Item>
+                    <Dropdown.Item disabled>Log out</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Menu.Menu>
+            </Container>
+          </Menu>
+        }
+        {role &&
+          <Container style={{ marginTop: '7em' }}>
+            <Route exact path='/' component={DoctorApp} />
+            { role_admin && <Route path='/doctors' component={Doctor} /> }
+            { role_admin && <Route path='/vehicles' component={Vehicle} /> }
+            <Route path='/doctor_apps' component={DoctorApp} />
+            <Route path='/vehicle_apps' component={VehicleApp} />
           </Container>
-        </Menu>
-        <Container style={{ marginTop: '7em' }}>
-          <Route exact path='/' component={Doctor} />
-          <Route path='/doctors' component={Doctor} />
-          <Route path='/vehicles' component={Vehicle} />
-          <Route path='/doctor_apps' component={DoctorApp} />
-          <Route path='/vehicle_apps' component={VehicleApp} />
-        </Container>
+        }
+        {!role &&
+          <Container style={{ marginTop: '7em' }}>
+            <Header as='h1'>Veterinarian POC</Header>
+            <Header as='h2'>Login as:</Header>
+            <Segment padded raised>
+              <Button primary fluid size='massive' onClick={this.onLoginRoleClick.bind(this, ROLE_ADMIN)}>Admin</Button>
+              <Divider horizontal>Or</Divider>
+              <Button secondary fluid size='massive' onClick={this.onLoginRoleClick.bind(this, ROLE_USER)}>User</Button>
+            </Segment>
+          </Container>
+        }
       </div>
     )
   }
